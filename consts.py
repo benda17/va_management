819980204c94b8d0889fab7667168b0743b56e67
benda17 <110421851+benda17@users.noreply.github.com>
2024-12-07 19:41:44 +0200

    Create consts.py
    The constants file

---
+++ consts.py
@@ -0,0 +1,28 @@
+SHEET_ID = "11P0rw8P4404OX31GAdP56wB9O2xDsKoIOZHuqRRkkgw"  # Beanda - Isaac's store
+DURATION_COLUMN_NAMES = ["Hours", "Hours:", "Hours worked"]
+DATE_COLUMN_NAMES = ["Day:", "Date", "Date:"]
+DATE_FORMATS = ["%d/%m/%Y", "%d-%-m/%Y", "%-d/%-m/%Y", "%d/%-m/%Y", "%-d-%-m/%Y"]
+# GID = 121796577
+
+# Info Table: Master sheet with all the information
+SHEETS_INFO_TABLE_KEY = "1zIZiBSGf4a-aj_cvvhIpQyr01ky4Xe9TDy2WYvwRTlg"
+SHEETS_INFO_TABLE_GID = 0
+LIMIT_COLUMN = "מכסת שעות נוכחית"
+FIRST_NAME_COLUMN = "שם פרטי של הלקוח"
+LAST_NAME_COLUMN = "שם משפחה של הלקוח"
+SHEET_ID_COLUMN = "מפתח גוגל שיטס"
+GID_COLUMN = "GID"
+PURCHASE_DATE_COLUMN = "תאריך רכישה"
+HOURS_SINCE_PAYMENT = "hours_since_pay"
+HOURS_PERCENT = "hours_percent"
+
+# EMOJIS
+YELLOW_BLOCK = '\U0001f7e8'
+RED_BLOCK = '\U0001f7e5'
+EXCLEMATION_MARK = "\U0000203C"
+OK_MARK = "\U00002714"
+
+
+# TELEGRAM INFO
+BOT_TOKEN = "5572194694:AAGvIhpGr_n7zcX8PlTruAZWk050apKmbSw"
+CHAT_ID = "-706589278"
